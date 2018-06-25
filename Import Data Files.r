bf64089f7b71acd30bc7fff57b1e84fbaf55fac6
KGS-Lab <40579531+KGS-Lab@users.noreply.github.com>
2018-06-25 13:36:17 -0700

    Update Import Data Files.r

--- Import Data Files.r
+++ Import Data Files.r
@@ -1,3 +1,8 @@
+# Instal the needed packages
+library(ggplot2)
+library(reshape2)
+
+# Import the data for the 6 evolutions
 read.table("/users/kerry/dropbox/pool_file_sum_all/Evol_14_RAD_R1.txt", sep = ",", header = TRUE)->A
 colnames(A)[2:length(colnames(A))]<-c(1,2,5,6,8,9,10,11,12,14)
 A$Condition <- "RAD"
@@ -29,9 +34,11 @@ A$Condition <- "M3"
 A$Replicate <- "Rep 1"
 Counts<-rbind(Counts, A)
 
+# Take a look at the table you built
 head(Counts)
 dim(Counts)
 
+# Restructure this table into a long format (instead of wide format above)
 Raw_Data<-melt(Counts, id = c('Condition', 'Replicate', 'BARCODE'))
 colnames(Raw_Data)[4:5]<-c("Timepoint", "Counts")
 
